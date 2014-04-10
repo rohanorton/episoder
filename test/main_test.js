138c499364c631b6b4d9b4c0997b8f26759c6857
Rohan Orton <rohan.orton@gmail.com>
2014-04-10 22:36:26 +0100

    getInfoFromFilename() matches more cases

--- test/main_test.js
+++ test/main_test.js
@@ -47,5 +47,34 @@ describe("main", function () {
       assert.strictEqual(result.episode, 4, "episode should be 4");
       done();
     });
+    it("should return object with show, season and episode numbers from filename in ### form", function (done) {
+      var result = main.getInfoFromFilename("Community 104.mp4");
+      assert.strictEqual(result.show, "Community", "show should be 'Community'");
+      assert.strictEqual(result.season, 1, "season should be 1");
+      assert.strictEqual(result.episode, 4, "episode should be 4");
+      done();
+    });
+    it("should return object with show, season and episode numbers from filename seperated by dots", function (done) {
+      var result = main.getInfoFromFilename("Parks.And.Recreation.S01E04.mp4");
+      assert.strictEqual(result.show, "Parks and Recreation", "show should be 'Parks and Recreation'");
+      assert.strictEqual(result.season, 1, "season should be 1");
+      assert.strictEqual(result.episode, 4, "episode should be 4");
+      done();
+    });
+    it("should return object with show, season and episode numbers even if there are numbers in the showname", function (done) {
+      var result = main.getInfoFromFilename("30 Rock S01E04.mp4");
+      assert.strictEqual(result.show, "30 Rock", "show should be '30 Rock'");
+      assert.strictEqual(result.season, 1, "season should be 1");
+      assert.strictEqual(result.episode, 4, "episode should be 4");
+      done();
+    });
+    it("should return object with show, season and episode numbers from filename with lots of trailing info", function (done) {
+      var result = main.getInfoFromFilename("Parks and Recreation S01E04.720p.HDTV.X264-DIMENSION.mp4");
+      assert.strictEqual(result.show, "Parks and Recreation", "show should be 'Parks and Recreation'");
+      assert.strictEqual(result.season, 1, "season should be 1");
+      assert.strictEqual(result.episode, 4, "episode should be 4");
+      done();
+    });
+
   });
 });
