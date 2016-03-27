43629c38e74b229b0380764da482c9f96cb79bfe
Rohan Orton <rohan.orton@gmail.com>
2016-03-27 17:38:56 +0100

    Use home-spun persistent caching instead of 3rd party module

--- src/cache.js
+++ src/cache.js
@@ -1,17 +1,32 @@
-import store from 'node-persist';
-import sanitize from 'sanitize-filename';
+import fs from 'fs-extra';
+import path from 'path';
 
-store.init();
+const getUserHome = () =>
+    process.env.HOME || process.env.USERPROFILE;
+
+const storeLocation = path.join(getUserHome(), '.episoder', 'cache');
+let store = {};
+
+try {
+    const persisted = fs.readFileSync(storeLocation);
+    store = JSON.parse(persisted);
+} catch (err) {
+    // ignore error
+}
+
+process.on('exit', () => {
+    fs.mkdirpSync(path.dirname(storeLocation));
+    fs.writeFileSync(storeLocation, JSON.stringify(store));
+});
 
 const cache = (fn) => {
     return async (key) => {
-        let safeKey = sanitize(key)
-        let data = store.getItem(safeKey);
+        let data = store[key];
         if (data) {
             return data;
         }
         data = await fn(key);
-        await store.setItem(safeKey, data);
+        store[key] = data;
         return data;
     }
 }
